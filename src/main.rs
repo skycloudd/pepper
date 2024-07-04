fc9d00b0d38d2208fdea9b1e81aaea6944b95eea
skycloudd <51929172+skycloudd@users.noreply.github.com>
2024-07-04 22:40:03 +0200

    small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use camino::Utf8PathBuf;
+use camino::{Utf8Path, Utf8PathBuf};
 use clap::Parser;
 use codespan_reporting::{
     files::SimpleFiles,
@@ -7,6 +7,7 @@ use codespan_reporting::{
         termcolor::{ColorChoice, StandardStream},
     },
 };
+use config::{Config, Linker, WithLinker};
 use core::{fmt::Write as _, str::FromStr as _};
 use owo_colors::OwoColorize as _;
 use pepper::{
@@ -15,11 +16,12 @@ use pepper::{
     lexer::tokens::FileId,
     SourceProgram,
 };
-use serde::Deserialize;
-use std::io::Write as _;
 #[cfg(unix)]
 use std::os::unix::process::ExitStatusExt as _;
+use std::{error::Error, io::Write as _};
 use target_lexicon::Triple;
+
+mod config;
 
 #[derive(Parser)]
 struct Args {
@@ -33,38 +35,6 @@ struct Args {
 
     #[clap(short, long)]
     config: Option<Utf8PathBuf>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Config {
-    target: Target,
-}
-
-#[derive(Debug, Deserialize)]
-struct Target {
-    #[serde(rename = "aarch64-apple-darwin")]
-    aarch64_apple_darwin: Aarch64AppleDarwin,
-}
-
-#[derive(Debug, Deserialize)]
-struct Aarch64AppleDarwin {
-    linker: Linker,
-}
-
-#[derive(Debug, Deserialize)]
-struct Linker {
-    command: String,
-    flags: Vec<String>,
-}
-
-trait TargetConfig {
-    fn linker(&self) -> &Linker;
-}
-
-impl TargetConfig for Aarch64AppleDarwin {
-    fn linker(&self) -> &Linker {
-        &self.linker
-    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -107,65 +77,76 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     if diagnostics.is_empty() {
         let target_config = match target_triple.to_string().as_str() {
-            "aarch64-apple-darwin" => {
-                &pepper_config.target.aarch64_apple_darwin as &dyn TargetConfig
-            }
+            "aarch64-apple-darwin" => &pepper_config.target.aarch64_apple_darwin as &dyn WithLinker,
             _ => return Err(format!("unsupported target triple: {target_triple}").into()),
         };
 
         let linker_config = target_config.linker();
 
-        let output_file = args.out.unwrap_or_else(|| "a.out".into());
-        let object_file = output_file.with_extension("o");
-        let program_file = output_file;
+        let output_filename = args.out.unwrap_or_else(|| "a.out".into());
+        let object_filename = output_filename.with_extension("o");
+        let program_filename = output_filename;
 
         let output = compile::get(&db, source_program, target_triple)
             .as_ref()
             .unwrap();
 
-        let mut output_file = std::fs::File::create(&object_file)?;
+        let mut output_file = std::fs::File::create(&object_filename)?;
         output_file.write_all(output)?;
 
         println!("Compilation successful");
 
-        println!(
-            "Linking with: {}",
-            format!(
-                "{} {object_file} -o {program_file}{}",
-                linker_config.command,
-                linker_config
-                    .flags
-                    .iter()
-                    .fold(String::new(), |mut init, flag| {
-                        write!(init, " {flag}").unwrap();
-                        init
-                    }),
-            )
-            .green()
-        );
-
-        let status = std::process::Command::new(&linker_config.command)
-            .arg(object_file)
-            .arg("-o")
-            .arg(&program_file)
-            .args(&linker_config.flags)
-            .status()?;
-
-        if !status.success() {
-            return Err(format!(
-                "{} exited with {}",
-                linker_config.command,
-                status.code().map_or_else(
-                    #[cfg(unix)]
-                    || format!("signal {}", status.signal().unwrap()),
-                    #[cfg(not(unix))]
-                    || "unknown exit code".to_string(),
-                    |code| format!("code {code}")
-                )
-            )
-            .into());
-        }
+        run_linker(linker_config, object_filename, program_filename)?;
     }
 
     Ok(())
+}
+
+fn run_linker<P: AsRef<Utf8Path>>(
+    linker_config: &Linker,
+    object_filename: P,
+    program_filename: P,
+) -> Result<(), Box<dyn Error>> {
+    let object_filename = object_filename.as_ref();
+    let program_filename = program_filename.as_ref();
+
+    println!(
+        "Linking with: {}",
+        format!(
+            "{} {object_filename} -o {program_filename}{}",
+            linker_config.command,
+            linker_config
+                .flags
+                .iter()
+                .fold(String::new(), |mut init, flag| {
+                    write!(init, " {flag}").unwrap();
+                    init
+                }),
+        )
+        .green()
+    );
+
+    let status = std::process::Command::new(&linker_config.command)
+        .arg(object_filename)
+        .arg("-o")
+        .arg(program_filename)
+        .args(&linker_config.flags)
+        .status()?;
+
+    if status.success() {
+        Ok(())
+    } else {
+        Err(format!(
+            "{} exited with {}",
+            linker_config.command,
+            status.code().map_or_else(
+                #[cfg(unix)]
+                || format!("signal {}", status.signal().unwrap()),
+                #[cfg(not(unix))]
+                || "unknown exit code".to_string(),
+                |code| format!("code {code}")
+            )
+        )
+        .into())
+    }
 }
