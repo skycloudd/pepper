0eb52884852a9b25f50681e8fdbe232cc9bd780c
skycloudd <51929172+skycloudd@users.noreply.github.com>
2024-11-26 18:03:36 +0100

    move tests to lib.rs (thanks bruh791 :3)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+use chumsky::{input::Input as _, span::Span as _, Parser as _};
+use diagnostics::error::{convert, Error};
 use lasso::ThreadedRodeo;
+use parser::ast::Ast;
+use span::{FileId, Span};
 use std::sync::LazyLock;
 
 pub mod diagnostics;
@@ -8,3 +12,68 @@ pub mod scopes;
 pub mod span;
 
 static RODEO: LazyLock<ThreadedRodeo> = LazyLock::new(ThreadedRodeo::new);
+
+#[must_use]
+pub fn parse_file(file_id: FileId, source: impl AsRef<str>) -> (Option<Ast>, Vec<Error>) {
+    let (tokens, lexer_errors) = lexer::lexer()
+        .parse(source.as_ref().with_context(file_id))
+        .into_output_errors();
+
+    let mut errors = lexer_errors
+        .iter()
+        .flat_map(|error| convert(error))
+        .collect::<Vec<_>>();
+
+    let (ast, parser_errors) = tokens.as_ref().map_or_else(
+        || (None, vec![]),
+        |tokens| {
+            let eoi = tokens
+                .last()
+                .map_or_else(|| Span::zero(file_id), |(_, span)| span.to_end());
+
+            parser::parser()
+                .parse(tokens.spanned(eoi))
+                .into_output_errors()
+        },
+    );
+
+    errors.extend(parser_errors.iter().flat_map(|error| convert(error)));
+
+    (ast, errors)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{parse_file, span::FileId};
+    use camino::Utf8PathBuf;
+    use std::fs::read_to_string;
+
+    #[test]
+    fn parser_tests() {
+        for entry in Utf8PathBuf::from("tests/parser").read_dir_utf8().unwrap() {
+            let entry = entry.unwrap();
+
+            assert!(entry.path().is_file(), "not a file: {:?}", entry.path());
+
+            assert!(
+                entry.path().extension().unwrap() == "pr",
+                "not a .pr file: {:?}",
+                entry.path()
+            );
+
+            let source = read_to_string(entry.path()).unwrap();
+
+            let (ast, errors) = parse_file(FileId::new(0), &source);
+
+            assert!(errors.is_empty(), "errors: {errors:#?}");
+
+            insta::with_settings!({
+                description => std::fs::read_to_string(entry.path()).unwrap().trim(),
+                info => &entry.path().to_string(),
+                snapshot_suffix => entry.path().file_stem().unwrap(),
+            }, {
+                insta::assert_yaml_snapshot!(ast.unwrap());
+            });
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use camino::Utf8PathBuf;
-use chumsky::{input::Input as _, span::Span as _, Parser as _};
 use clap::Parser;
 use codespan_reporting::{
     files::SimpleFiles,
@@ -9,13 +8,8 @@ use codespan_reporting::{
     },
 };
 use pepper::{
-    diagnostics::{
-        error::{convert, Error},
-        report::report,
-    },
-    lexer,
-    parser::{self, ast::Ast},
-    span::{FileId, Span},
+    diagnostics::{error::Error, report::report},
+    span::FileId,
 };
 use std::{fs::read_to_string, process::ExitCode};
 
@@ -29,7 +23,10 @@ fn main() -> ExitCode {
 
     let mut files = SimpleFiles::new();
 
-    let (ast, errors) = parse_file(args.filename, &mut files);
+    let source = read_to_string(&args.filename).unwrap();
+    let file_id = FileId::new(files.add(args.filename, source.clone()));
+
+    let (ast, errors) = pepper::parse_file(file_id, source);
 
     eprintln!("ast: {ast:#?}");
 
@@ -41,43 +38,6 @@ fn main() -> ExitCode {
     }
 }
 
-fn parse_file(
-    filename: impl Into<Utf8PathBuf>,
-    files: &mut SimpleFiles<Utf8PathBuf, String>,
-) -> (Option<Ast>, Vec<Error>) {
-    let filename = filename.into();
-
-    let source = read_to_string(&filename).unwrap();
-
-    let file_id = FileId::new(files.add(filename, source.clone()));
-
-    let (tokens, lexer_errors) = lexer::lexer()
-        .parse(source.with_context(file_id))
-        .into_output_errors();
-
-    let mut errors = lexer_errors
-        .iter()
-        .flat_map(|error| convert(error))
-        .collect::<Vec<_>>();
-
-    let (ast, parser_errors) = tokens.as_ref().map_or_else(
-        || (None, vec![]),
-        |tokens| {
-            let eoi = tokens
-                .last()
-                .map_or_else(|| Span::zero(file_id), |(_, span)| span.to_end());
-
-            parser::parser()
-                .parse(tokens.spanned(eoi))
-                .into_output_errors()
-        },
-    );
-
-    errors.extend(parser_errors.iter().flat_map(|error| convert(error)));
-
-    (ast, errors)
-}
-
 fn emit_errors(errors: &[Error], files: &SimpleFiles<Utf8PathBuf, String>) {
     let writer = StandardStream::stderr(ColorChoice::Auto);
     let term_config = term::Config::default();
@@ -86,41 +46,5 @@ fn emit_errors(errors: &[Error], files: &SimpleFiles<Utf8PathBuf, String>) {
         let diagnostic = report(error);
 
         term::emit(&mut writer.lock(), &term_config, files, &diagnostic).unwrap();
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::parse_file;
-    use camino::Utf8PathBuf;
-    use codespan_reporting::files::SimpleFiles;
-
-    #[test]
-    fn parser_tests() {
-        let mut files = SimpleFiles::new();
-
-        for entry in Utf8PathBuf::from("tests/parser").read_dir_utf8().unwrap() {
-            let entry = entry.unwrap();
-
-            assert!(entry.path().is_file(), "not a file: {:?}", entry.path());
-
-            assert!(
-                entry.path().extension().unwrap() == "pr",
-                "not a .pr file: {:?}",
-                entry.path()
-            );
-
-            let (ast, errors) = parse_file(entry.path(), &mut files);
-
-            assert!(errors.is_empty(), "errors: {errors:#?}");
-
-            insta::with_settings!({
-                description => std::fs::read_to_string(entry.path()).unwrap().trim(),
-                info => &entry.path().to_string(),
-                snapshot_suffix => entry.path().file_stem().unwrap(),
-            }, {
-                insta::assert_yaml_snapshot!(ast.unwrap());
-            });
-        }
     }
 }
